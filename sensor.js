0deda8944857b167e65776986fdffeb1b8c213a9
Anurag78tl <anuragshrm70@gmail.com>
2019-06-30 23:44:27 +0530

    ________________

--- sensor.js
+++ sensor.js
@@ -1,8 +1,23 @@
 var fs = require("fs");
+class Sensor {
+	public static const SENSOR
+	private shellCommand, sensorUrl, sensorEnv;
+	private sensorType;
+	public constructor(data) {
+
+	}
+	public getData() {
+
+	}
+};
 function SensorConfig(onBuild) {
   fs.readFile("SensorConfig.json", function(err, data) {
-    if(err) {
-    this.buildConfig(JSON.parse(data));
+    if(!err) {
+    	this.buildConfig(JSON.parse(data));
+    	onBuild(null);
+    }
+    else {
+    	onBuild(err);
     }
   });
 }
@@ -10,5 +25,10 @@ SensorConfig.prototype.config = null;
 SensorConfig.prototype.buildConfig = function(config) {
   this.config = config;
 }
-SensorConfig.prototype.
+SensorConfig.prototype.getSensor = async function(sensorName) {
+	if(!this.config[sensorName]) {
+		throw new Error("Sensor " + sensorName + " is not present");
+		return new Sensor(this.config);
+	}
+}
 
